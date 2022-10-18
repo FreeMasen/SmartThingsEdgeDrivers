bb62a3c02eba36ca254955df6b303f537906be7e
Robert Masen <robert.masen@smartthings.com>
2022-10-18 13:51:20 -0500

    streamline hub-id's for install

--- publish.sh
+++ publish.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 exit_on_error() {
-    exit_code=$1
-    last_command=${@:2}
-    echo "exit code: $1"
+
     if [[ $exit_code -ne 0 ]]; then
-        >&2 echo "\"${last_command}\" command failed with exit code ${exit_code}."
+        >&2  echo "command failed with exit code $1."
         exit $exit_code
     fi
     echo "successful"
@@ -16,6 +14,12 @@ shift
 INSTALL_HUB_ID=0
 PROD_CHANNEL_ID="2076fb52-0ef5-4db9-aa7d-34fb6de0b6a8"
 BETA_CHANNEL_ID="b9930d5a-f3f5-4428-b378-b12cb3d93093"
+
+if test -f "ids_env"; then
+    source "ids_env"
+fi
+
+
 CHANNEL_ID=$PROD_CHANNEL_ID
 parse_args() {
     while [ $# -gt 0 ]; do
@@ -23,7 +27,20 @@ parse_args() {
         echo "$key"
         case "$key" in
             --install)
-                INSTALL_HUB_ID="$2"
+                case $2 in
+                    personal)
+                        INSTALL_HUB_ID=$PERSONAL_HUB_ID
+                        ;;
+                    v3)
+                        INSTALL_HUB_ID=$PROD_V3_HUB_ID
+                        ;;
+                    v2)
+                        INSTALL_HUB_ID=$PROD_V2_HUB_ID
+                        ;;
+                    *)
+                        INSTALL_HUB_ID="$2"
+                    ;;
+                esac
                 shift
                 ;;
             --beta)
@@ -37,16 +54,16 @@ parse_args() {
 }
 parse_args $@
 echo "packaging $PACKAGE_PATH"
-
-ID="$(st edge:drivers:package $PACKAGE_PATH --json | jq -r ".driverId")"
+PACKAGE_JSON=st edge:drivers:package $PACKAGE_PATH --json
+echo $?
 exit_on_error $?
+ID="$(echo "$PACKAGE_JSON" | jq -r ".driverId")"
 echo "publishing $ID to $CHANNEL_ID"
 st edge:drivers:publish $ID -C $CHANNEL_ID
-exit_on_error $?
+exit_on_error $? "$(echo "!:0")" "!:*"
 
 if [ ! -z "$INSTALL_HUB_ID" ]; then
     echo "Installing $ID $INSTALL_HUB_ID $CHANNEL_ID"
     st edge:drivers:install "$ID" -H="$INSTALL_HUB_ID" -C="$CHANNEL_ID"
-    exit_on_error $?
+    exit_on_error $? "!:0" "!:*"
 fi
-
