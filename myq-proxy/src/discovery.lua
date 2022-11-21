5074bf6f85a9374fe26471b2e4db23f0498d721d
Robert Masen <r.f.masen@gmail.com>
2022-11-20 20:34:15 -0600

    compile teal

--- myq-proxy/src/discovery.lua
+++ myq-proxy/src/discovery.lua
@@ -1,116 +1,127 @@
-local cosock = require "cosock"
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local pcall = _tl_compat and _tl_compat.pcall or pcall; local cosock = require("cosock")
 local socket = cosock.socket
-local json = (function()
-    local s, m = pcall(require, "st.json")
-    if s then return m end
-    return require "dkjson"
-end)()
+local json = require("st.json")
 
---- Create the UDP socket used in our not-ssdp discovery protocol
---- @return cosock.socket.udp
+
+
 local function create_udp_socket()
-  local s = socket.udp()
-  assert(s)
-  local listen_ip = "0.0.0.0"
-  local listen_port = 0
+   local s = socket.udp()
+   assert(s)
+   local listen_ip = "0.0.0.0"
+   local listen_port = 0
 
-  assert(s:setsockname(listen_ip, listen_port))
-  s:settimeout(8)
-  return s
+   assert(s:setsockname(listen_ip, listen_port))
+   s:settimeout(8)
+   return s
 end
 
---- Generate the query string for our not-ssdp discovery protocol
---- @return string
+
+
 local function create_query()
-  return json.encode({
-    target = "myq-proxy",
-    source = "st-driver",
-  })
+   return json.encode({
+      target = "myq-proxy",
+      source = "st-driver",
+   })
 end
 
---- Send the not-ssdp query to our multicast group
---- @param s {cosock.socket.udp} The socket to send on (default will be created if `nil`)
+
+
 local function send_query(s)
-  if not s then
-    s = create_query()
-  end
-  local multicast_ip = "239.255.255.250"
-  local multicast_port = 1919
-  local multicast_msg = create_query()
-  assert(s:sendto(multicast_msg, multicast_ip, multicast_port))
+   if not s then
+      s = create_udp_socket()
+   end
+   local multicast_ip = "239.255.255.250"
+   local multicast_port = 1919
+   local multicast_msg = create_query()
+   assert(s:sendto(multicast_msg, multicast_ip, multicast_port))
 end
 
---- Wait for the not-ssdp reply or a cancel signal from the rx 
---- 
---- Returns `string, string, integer` on success otherwise returns `nil, string`
---- @param s {cosock.socket.udp}
---- @return string|nil, string, integer|nil
-local function wait_for_reply(s, rx)
-  local rxs, _, err = cosock.socket.select({s, stop_rx})
-  if rxs[1] == s or rxs[2] == s then
-    return s:receivefrom()
-  end
-  err = err or "timeout"
-  if rxs[1] == rx or rxs[2] == rx then
-    err = "stopped"
-  end
-  return nil, nil, err
+
+
+
+
+
+local function wait_for_reply(s, stop_rx)
+   local rxs, _, err = cosock.socket.select({ s, stop_rx })
+   if type(rxs) == "nil" then
+      return nil, err
+   end
+   local r = rxs
+   if r[1] == s or r[2] == s then
+      return s:receivefrom()
+   end
+   err = err or "timeout"
+   if r[1] == stop_rx or r[2] == stop_rx then
+      err = "stopped"
+   end
+   return nil, err
 end
 
---- Send and recieve a query one time
---- @param s {cosock.socket.udp} UDP socket to query
---- @param stop_rx {cosock.channel} Channel to listen for cancel signals on
---- @return string|nil, string, integer|nil
+
+
+
+
 local function query_once(s, stop_rx)
-  send_query(s)
-  return wait_for_reply(s, stop_rx)
+   send_query(s)
+   return wait_for_reply(s, stop_rx)
 end
 
---- Spawn a cosock task that probes the provided callback and sleeps for 1 second
---- in a loop until the provided callback returns `false` at which point it will send
---- `nil` on the tx paired with the returned rx
---- @param should_continue {fn():bool} The callback to probe
---- @return {cosock.channel}
+
+
+
+
+
 local function spawn_stop_task(should_continue)
-  local tx, rx = cosock.channel.new()
-  cosock.spawn(function()
-    while should_continue() do
-      cosock.socket.sleep(1)
-      if tx.link.closed then
-        break
-      end 
-    end
-    tx:send()
-  end)
-  return rx
+   local tx, rx = cosock.channel.new()
+   cosock.spawn(function()
+      while should_continue() do
+         cosock.socket.sleep(1)
+         if tx.link.closed then
+            break
+         end
+      end
+      tx:send()
+   end)
+   return rx
 end
 
---- Attempt to discover the proxy server on the local network. This function will either yield
---- until an expected reply is received or until a provided callback returns `false`
---- @param should_continue {fun():bool}
---- @return {ip: string, port: integer}|nil, nil|string
-local function discover_proxy_server(should_continue)
-  should_continue = should_continue or function() true end
-  local stop_rx = spawn_stop_task(should_continue)
-  local s = create_udp_socket()
-  while true do
-    local msg, ip, port = query_once(s)
-    if msg then
-      local success, t = pcall(json.decode, msg)
-      if success and t.ip and t.port then
-        stop_rx:close()
-        return t
+
+
+
+
+
+local function default_continue()
+   return true
+end
+
+
+
+
+
+local function discover_proxy_server(
+   should_continue)
+
+   should_continue = should_continue or default_continue
+   local stop_rx = spawn_stop_task(should_continue)
+   local s = create_udp_socket()
+   while true do
+      local msg, ip, _port = query_once(s)
+      if msg then
+         local success, t = pcall(json.decode, msg)
+         if success and t.ip and t.port then
+            stop_rx:close()
+            return t
+         end
+      elseif ip ~= "timeout" then
+
+         return nil, ip
       end
-    else if ip ~= "timeout" then
-      -- ip is an error message here and port is probably nil
-      return nil, ip, port
-    end
-  end
-  stop_rx:close()
-  return nil, "was stopped"
+   end
+   stop_rx:close()
+   return nil, "was stopped"
 end
 
 return {
-  query_once = query_once,
-  discover_proxy_server = discover_proxy_server,
+   query_once = query_once,
+   discover_proxy_server = discover_proxy_server,
 }
--- myq-proxy/src/task.lua
+++ myq-proxy/src/task.lua
@@ -1,93 +1,132 @@
-local cosock = require "cosock"
-local log = require "log"
-local lustre = require "lustre"
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local cosock = require("cosock")
+local log = require("log")
+local lustre = require("lustre")
+local json = require("st.json")
 
-local json = (function()
-    local s, m = pcall(require, "st.json")
-    if s then return m end
-    return require "dkjson"
-end)()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 local function generate_ws_command_msg(device_id, command)
-    return json.encode({
-        type = "ExecuteCommand",
-        device_id = device_id,
-        command = command,
-    })
+   return {
+      type = "ExecuteCommand",
+      device_id = device_id,
+      command = command,
+   }
 end
 
-local function generate_open(device_id)
-    return generate_ws_command_msg(device_id, "open")
+local function generate_ws_open(device_id)
+   return generate_ws_command_msg(device_id, "open")
 end
 
 local function generate_ws_close(device_id)
-    return generate_ws_command_msg(device_id, "close")
+   return generate_ws_command_msg(device_id, "close")
 end
 
 local function generate_ws_on(device_id)
-    return generate_ws_command_msg(device_id, "on")
+   return generate_ws_command_msg(device_id, "on")
 end
 
 local function generate_ws_off(device_id)
-    return generate_ws_command_msg(device_id, "off")
+   return generate_ws_command_msg(device_id, "off")
 end
 
 local function generate_ws_get_devices()
-    return json.encode({
-        type = "GetDevices",
-    })
+   return {
+      type = "GetDevices",
+   }
 end
 
 local function generate_ws_get_device(device_id)
-    return json.encode({
-        type = "GetDevice",
-        device_id = device_id,
-    })
+   return {
+      type = "GetDevice",
+      device_id = device_id,
+   }
 end
 
-local function generate_ws_get_devices()
-    return json.encode({
-        type = "GetDevices",
-    })
-end
+local function spawn_ws_task(
+   cmd_rx,
+   cmd_tx,
+   ip,
+   port)
 
-local function spawn_ws_task(cmd_rx, cmd_tx, ip, port)
-    local ws = lustre.WebSocket.client(assert(cosock.socket.tcp()), "/event-stream", lustre.Config.default())
-    cosock.spawn(function()
-        print("connecting")
-        assert(ws:connect(ip, port))
-        print("connected")
-        while true do
-            print("selecting")
-            local rcvrs, _, err = cosock.socket.select({ws, cmd_rx})
-            print("selected")
-            if rcvrs[1] == ws or rcvrs[2] == ws then
-                local msg, err = ws:receive()
-                if err and err ~= "timeout" then
-                    cmd_tx:send({
-                        type = "Error",
-                        err = err,
-                        level = "Fatal"
-                    })
-                    return log.error("Error in websocket task", err)
-                end
-                cmd_tx:send(json.decode(msg.data))
+   local ws = lustre.WebSocket.client(assert(cosock.socket.tcp()), "/event-stream", lustre.Config.default())
+   cosock.spawn(function()
+      print("connecting")
+      assert(ws:connect(ip, port))
+      print("connected")
+      while true do
+         print("selecting")
+         local rcvrs, _, select_err = cosock.socket.select({ ws, cmd_rx })
+         print("selected")
+         if type(rcvrs) == "nil" then
+            goto continue
+         end
+         local r = rcvrs
+         if (r[1] == ws or r[2] == ws) then
+            local msg, ws_err = ws:receive()
+            if type(msg) == "nil" and ws_err and ws_err ~= "timeout" then
+               cmd_tx:send({
+                  type = "Error",
+                  err = ws_err,
+                  level = "Fatal",
+               })
+               log.error("Error in websocket task", ws_err)
+               return
             end
-            if rcvrs[1] == cmd_rx or rcvrs[2] == cmd_rx then
-                local msg, err = cmd_rx:receive()
-                print("cmd", msg, err)
-                ws:send_text(msg)
+            cmd_tx:send(json.decode((msg).data))
+         end
+         if r[1] == cmd_rx or r[2] == cmd_rx then
+            local msg, ch_err = cmd_rx:receive()
+            print("cmd", msg, ch_err)
+            if msg then
+               ws:send_text(json.encode(msg))
+            else
+               cmd_tx:send({
+                  type = "Error",
+                  err = ch_err,
+                  level = "Warning",
+               })
             end
-        end
-    end)
+         end
+         if select_err ~= "timeout" then
+            cmd_tx:send({
+               type = "Error",
+               err = select_err,
+               level = "Warning",
+            })
+         end
+         ::continue::
+      end
+   end)
 end
 
 return {
-    generate_ws_close = generate_ws_close,
-    generate_ws_open = generate_ws_open,
-    generate_ws_on = generate_ws_on,
-    generate_ws_off = generate_ws_off,
-    generate_ws_get_devices = generate_ws_get_devices,
-    generate_ws_get_device = generate_ws_get_device,
-    spawn_ws_task = spawn_ws_task,
+   generate_ws_close = generate_ws_close,
+   generate_ws_open = generate_ws_open,
+   generate_ws_on = generate_ws_on,
+   generate_ws_off = generate_ws_off,
+   generate_ws_get_devices = generate_ws_get_devices,
+   generate_ws_get_device = generate_ws_get_device,
+   spawn_ws_task = spawn_ws_task,
 }
