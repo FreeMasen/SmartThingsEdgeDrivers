0d84b66ad5758ef9f6423a4ebcef82f55db3f724
Robert Masen <robert.masen@smartthings.com>
2023-06-28 10:50:24 -0500

    add info changed print

--- ch_hand/src/init.lua
+++ ch_hand/src/init.lua
@@ -4,16 +4,17 @@ local caps = require 'st.capabilities'
 local discovery = require "disco"
 local utils = require "st.utils"
 
-local cosock = require "cosock"
 local server = require "server"
 
 local function device_added(driver, device)
-    log.warn("device_added", device.label or device.id)
+    log.trace("device_added", device.label or device.id)
     device:emit_event(caps.switch.switch.off())
     device:emit_event(caps.valve.valve.closed())
 end
 
 local function info_changed(driver, device)
+    log.trace("info_changed", device.label or device.id)
+    log.debug(utils.stringify_table(device.preferences, "preferences", true))
 end
 
 local d = Driver("ch_hand", {
@@ -23,9 +24,6 @@ local d = Driver("ch_hand", {
         infoChanged = info_changed,
     },
     discovery = discovery.disco_handler,
-    })
+})
 server(d)
-d:call_on_schedule(60, function()
-    cosock.socket.sleep(5)
-end, "call_on_schedule")
 d:run()
